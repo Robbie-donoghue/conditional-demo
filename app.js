ba5c9846ebed733b06375520be56282bf9e25e37
Robbie <Leon.Wh1te@proton.me>
2024-01-11 15:14:11 +0000

    new commit message

--- app.js
+++ app.js
@@ -5,16 +5,10 @@ const drinkAge = 18;
 let isdrinkLegal= false;
 
 
-if(age > drinkAge)
-{
-    console.log("You can drink");
-}
-
-
-
 if(age >= drinkAge)
 {
     isdrinkLegal= true;
+    console.log("Can I have a drink", isdrinkLegal)
 }
 console.log("Can I have a drink", isdrinkLegal)
 
