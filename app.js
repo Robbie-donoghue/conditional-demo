eb3ecfd03a5314a396c0d3119a29f8cd28962bc9
Robbie <Leon.Wh1te@proton.me>
2024-01-11 14:47:43 +0000

    this is the conditional demo

---
+++ app.js
@@ -0,0 +1,20 @@
+let age = 18;
+
+const drinkAge = 18;
+
+let isdrinkLegal= false;
+
+
+if(age > drinkAge)
+{
+    console.log("You can drink");
+}
+
+
+
+if(age >= drinkAge)
+{
+    isdrinkLegal= true;
+}
+console.log("Can I have a drink", isdrinkLegal)
+
